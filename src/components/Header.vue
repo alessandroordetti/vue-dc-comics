9c6951962668556c035c8edf788dc5a67f4ae340
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-28 17:24:44 +0200

    Header completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,62 @@
 <template>
-    <div>
-        <h2>Header</h2>
+    <div class="header-wrapper">
+        <div class="img-wrapper">
+            <img src="../assets/img/dc-logo.png" alt="Logo DC">
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        Characters
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Comics
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Movies
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                    Games
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Collectibles
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        Videos
+                    </a>
+                </li>
+                <li><a href="">
+                    Fans
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        News
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        Shop
+                    </a> 
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -10,6 +66,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    .header-wrapper {
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
 
+        & .img-wrapper img {
+            width: 70%;
+        }
+    }
+    
+    nav {
+        display: flex;
+        justify-content: space-evenly;
+
+        & ul {
+        display: flex;
+        align-items: center;
+
+        & li {
+            list-style: none;
+            padding: .6rem;
+        }
+
+        & a{
+            text-transform: uppercase;
+            color: black;
+            text-decoration: none;
+        }
+        }
+    }
 </style>
