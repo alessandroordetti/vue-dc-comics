bafc31ef6639261904a945dcafc459dc18e00518
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-29 16:59:40 +0200

    Props added

---
+++ src/components/productCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="comics-card">
+        <img :src="thumb" :alt="series">
+        <h3>{{series}} </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'productCard',
+    props: ['thumb', 'price', 'series', 'type']
+}
+</script>
+
+<style lang="scss" scoped>
+                .comics-card {
+                    margin-right: 15px;
+                    width: calc((100% - 90px) / 6);
+                    img {
+                        width: 100%;
+                        height: 250px;
+                    }
+                }
+</style>
